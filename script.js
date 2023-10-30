68043970552b093e04508ae8d10d9b511bea28d4
Britteany <miszbritteany@gmail.com>
2023-10-29 22:07:30 -0400

    made form for user input and added button styling

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
  }
+//Take user's input and store the new book objects into an array
+
+ const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // the constructor...
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+//Click button to bring up user input as a form
+
