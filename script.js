9cd586d1e6e8c6704ee26c3983b092023d2010f6
Britteany <miszbritteany@gmail.com>
2023-10-30 23:44:31 -0400

    hid sidebar and revealed it on click "+Add Book"

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Take user's input and store the new book objects into an array
 
- const myLibrary = [];
+//const myLibrary = [];
 
-function Book(title, author, pages, read) {
+/*function Book(title, author, pages, read) {
   // the constructor...
   this.title = title
   this.author = author
@@ -16,5 +16,19 @@ function addBookToLibrary() {
 
 }
 
-//Click button to bring up user input as a form
+//Collapse and expand sidebar*/
+
+//Create alert for form inputs
+let newBook = document.querySelector(".new-book");
+newBook.addEventListener("click", function() {
+document.querySelector(".side-bar").style.display = "block";
+});
+
+let formSubmit = document.querySelector(".form-submit");
+formSubmit.addEventListener("click", function() {
+  alert("hello")
+});
+
+
+
 
