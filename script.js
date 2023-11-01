fa4127c2023440325de2384e4361cb7eb8bacd9d
Britteany <miszbritteany@gmail.com>
2023-10-31 21:51:16 -0400

    hide sidebar on click submit. form info works.

--- script.js
+++ script.js
@@ -2,31 +2,35 @@
 
 //const myLibrary = [];
 
-/*function Book(title, author, pages, read) {
+function Book(title, author, pages, read) {
   // the constructor...
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
-  let submit = document.getElementsByClassName("form-submit");
-  submit.submit();
-
+  // do stuff here
+  let title = document.querySelector(".title").value;
+  let author = document.querySelector(".author").value;
+  let pages = document.querySelector(".pages").value;
+  let read = document.querySelector("#checked").checked;
+  let bookInfo = new Book(title, author, pages, read);
+  return bookInfo;
 }
 
-//Collapse and expand sidebar*/
-
-//Create alert for form inputs
 let newBook = document.querySelector(".new-book");
 newBook.addEventListener("click", function() {
 document.querySelector(".side-bar").style.display = "block";
 });
 
 let formSubmit = document.querySelector(".form-submit");
-formSubmit.addEventListener("click", function() {
-  alert("hello")
+formSubmit.addEventListener("click", function(event) {
+  event.preventDefault();
+  console.log(addBookToLibrary())
+  document.querySelector(".side-bar").style.display = "none";
+  //addBookToLibrary()
 });
 
 
